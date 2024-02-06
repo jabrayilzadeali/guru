ab4055514e0e239da1c34100cc521f99fbb20629
Jabrayilzade Ali <jabrayilzadeali@protonmail.com>
2024-02-06 16:43:54 +0400

    some changes

--- backend/course/models.py
+++ backend/course/models.py
@@ -28,12 +28,33 @@ class CourseContent(models.Model):
 
     content_type = models.CharField(max_length=10, choices=[('text', 'Text'), ('video', 'Video')])
     content = models.TextField()
+    
+    is_free = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return f"Content by {self.user.username} on {self.course.title}"
+    
+
+class ContentProgress(models.Model):
+    student = models.ForeignKey("Student", on_delete=models.CASCADE)
+    content = models.ForeignKey("CourseContent", on_delete=models.CASCADE)
+    progress = models.CharField(max_length=11, choices=[('not_started', 'not started'), ('started', 'Started'), ('finished', 'Finished')])
+    
+    def __str__(self):
+        return f"Content Progress {self.progress}"
+
+
+class Student(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Student {self.user}"
+    
+    
     
 
 
