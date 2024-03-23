8d9d60210ef280720b54553abc5683d85e89a727
Jabrayilzade Ali <jabrayilzadeali@protonmail.com>
2024-03-24 02:02:32 +0400

    added scripts/reset_django.sh file

--- backend/scripts/reset_django.sh
+++ backend/scripts/reset_django.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+pwd
+
 # Remove SQLite database
 rm db.sqlite3
 
-# Remove pycache
-rg --files --hidden --glob '!.git/*' '(__pycache__|\.pyc|\.pyo$)' | xargs rm -rf
+# Remove pycache directories
+find . -type d -not -path "./.venv/*" -name "__pycache__" -exec rm -rf {} +
 
-# Remove migrations
-rg --files --hidden --glob '!.git/*' '*.py' | rg -v '__init__.py' | rg 'migrations' | xargs rm -rf
+# Remove migration files except __init__.py
+find . -not -path "./.venv/*" -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -not -path "./.venv/*" -path "*/migrations/*.pyc" -delete
 
 # Run migrations
-python manage.py makemigrations
-python manage.py migrate
+# python manage.py makemigrations
+# python manage.py migrate
